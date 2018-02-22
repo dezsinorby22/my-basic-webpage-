14384a1bfed7a9b184dfbc8b42166f53f532b143
Unknown <xnorby95x@gmail.com>
2018-02-22 22:58:54 +0100

    basic js ,css, live server
    js,live server

---
+++ app.js
@@ -0,0 +1,41 @@
+/*global $, jQuery, alert*/
+
+
+// minden ami ebben a blokkban van az strict mode-ban lesz
+// a strict mode egy szigorubb mukodest eredmenyez, tobb esetben
+// fog hibat jelezni a bongeszo (es kevesebb lesz a nem vart munkodes)
+// read more: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+(function () {
+// strict mode bekapcsolasa:
+'use strict';
+
+
+
+
+// kivalasztunk minden html elemet amin rajta van az image popup class
+// es meghivjuk rá a magnific popup fugvenyt
+// a fugveny bemeneti parametere egy object
+// ami a popup beallitasait tartalmazza
+$('.image-popup').magnificPopup({
+  type: 'image',
+  gallery: {
+    enabled: true
+  }
+});
+
+
+
+// minden hidden-image class-al rendelkező elemet elrejtünk
+// sima javascriptet használunk ehhez:
+[].forEach.call(document.querySelectorAll('.hidden'),
+  function (el) {
+    el.style.display = 'none';
+});
+
+
+// ez ugyanazt csinálja mint a fentebbi kód, csak ez
+// a jquery library segítségével:
+$('.hidden').css('display', 'none');
+
+
+}()); // end of strict mode
